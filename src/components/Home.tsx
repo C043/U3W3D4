99f56c1377e48459c839a1261731511db4c20ea3
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-11 14:33:41 +0200

    Adds home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
 import SpaceNav from "./SpaceNav";
+import { Container, Row, Spinner } from "react-bootstrap";
+import { News } from "../interfaces/iNews";
+import SingleNews from "./SingleNews";
 
 const Home = () => {
-  return <SpaceNav />;
+  const [news, setNews] = useState<News[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchNews = async () => {
+    try {
+      const resp = await fetch("https://api.spaceflightnewsapi.net/v4/articles");
+      if (resp.ok) {
+        const news = await resp.json();
+        setNews(news.results);
+      } else {
+        throw new Error("Errore nel fetch");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
+  return (
+    <>
+      <SpaceNav />
+      <Container>
+        <h1 className="my-3">Latest News</h1>
+        <Row className="mb-5 g-2">
+          {isLoading ? (
+            <Spinner variant="primary" />
+          ) : (
+            news.map((news: News) => <SingleNews key={news.id} news={news} />)
+          )}
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/SingleNews.tsx
@@ -0,0 +1,27 @@
+import { Col } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import { News } from "../interfaces/iNews";
+import { NavLink } from "react-router-dom";
+
+interface SingleNewsProp {
+  news: News;
+}
+
+function SingleNews({ news }: SingleNewsProp) {
+  return (
+    <Col xs="3">
+      <Card>
+        <Card.Img variant="top" src={news.image_url} className="fixed-height" />
+        <Card.Body>
+          <Card.Title className="text-truncate">{news.title}</Card.Title>
+          <Card.Text className="line-clamp-3">{news.summary}</Card.Text>
+          <NavLink to={"/article/" + news.id} className="btn btn-dark">
+            Go to article
+          </NavLink>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
+
+export default SingleNews;
